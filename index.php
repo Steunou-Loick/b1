5985175aabde3f4fc6d82c941067c6579e08541f
boismond-j <boismond-j@saint-louis29.net>
2021-10-14 08:53:19 +0200

    commit test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 ?>
 <html>
 <head>
-	<title>teeeeeeeeeeest location appartement</title>
+	<title>Location appartement</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="FichierDeStyle.css">
